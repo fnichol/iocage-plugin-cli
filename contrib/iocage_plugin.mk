39b5478e45685e0b5e20e5676b75fb79266b4997
Fletcher Nichol <fnichol@nichol.ca>
2021-07-05 15:07:48 -0600

    feat: add a vendorable iocage_plugin.mk Makefile target
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ contrib/iocage_plugin.mk
@@ -0,0 +1,10 @@
+CLI_INSTALL_SRC_URL := https://fnichol.github.io/iocage-plugin-cli/install.sh
+VENDORED_CLI := overlay/usr/local/bin/plugin
+
+vendor-cli: ## Vendors the current version of the iocage-plugin-cli
+	@echo "--- $@"
+	curl -sSfL $(CLI_INSTALL_SRC_URL) \
+		| sh -s -- --destination=$$(dirname $(VENDORED_CLI)) \
+		&& $(VENDORED_CLI) --version \
+		&& echo "--- New version of iocage-plugin-cli is vendored in $(VENDORED_CLI)"
+.PHONY: vendor-cli
